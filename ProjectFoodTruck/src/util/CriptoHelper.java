13d673b15bcf7b510d84e3399f7711c9aa0a568c
Spock <Spock@DESKTOP-P83V992>
2018-06-02 15:06:20 -0300

    Adicionando funcionalidade de salvar a chave no servidor

--- ProjectFoodTruck/src/util/CriptoHelper.java
+++ ProjectFoodTruck/src/util/CriptoHelper.java
@@ -1,5 +1,8 @@
 package util;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.KeyPair;
@@ -18,11 +21,11 @@ import javax.crypto.SecretKey;
 
 import org.apache.commons.codec.binary.Base64;
 
-public class CriptoHelper {	
+public class CriptoHelper {
 
 	public static String criptografar(String puroTexto) {
 		try {
-			return CifraSimetrica.criptografar(puroTexto, "CHAVE");
+			return CifraSimetrica.criptografar(puroTexto, lerArquivoChave());
 		} catch (Exception e) {
 			return null;
 		}
@@ -30,28 +33,58 @@ public class CriptoHelper {
 
 	public static String descriptografar(String cifroTexto) {
 		try {
-			return CifraSimetrica.descriptografar(cifroTexto, "CHAVE");
+			return CifraSimetrica.descriptografar(cifroTexto, lerArquivoChave());
 		} catch (Exception e) {
 			return null;
 		}
 	}
-	
-	public static void main(String[] args) throws Exception {
-		String puroTexto = "Este é o puro texto";	
-		System.out.println("Texto: " + puroTexto);
+
+	private static String lerArquivoChave() {
+		BufferedReader leitor = null;
+		try  {
+		    File arquivo = new File("C:\\");
+        	if (!arquivo.exists())
+        		arquivo.mkdir();
+
+            arquivo = new File(arquivo, "temp.txt");
+            
+			leitor = new BufferedReader(new FileReader(arquivo));
+
+			StringBuilder texto = new StringBuilder();
+	        String linha;
+	        while ((linha = leitor.readLine()) != null){
+               texto.append(linha);
+	        }
 		
+	        leitor.close();
+	        
+	        return CifraBase64.descriptografar(texto.toString());
+		} catch (Exception e) {
+			try {
+				leitor.close();				
+			} catch (Exception e2) {
+			}
+		}
+			
+		return null;
+	}
+
+	public static void main(String[] args) throws Exception {
+		String puroTexto = "Este é o puro texto";
+		System.out.println("Texto: " + puroTexto);
+
 		System.out.print("Criptografar: ");
 		String cifroTexto = CriptoHelper.criptografar(puroTexto);
 		System.out.println(cifroTexto.toString());
-		
+
 		System.out.print("Decriptografar: ");
 		String novoPuroTexto = CriptoHelper.descriptografar(cifroTexto);
 		System.out.println(novoPuroTexto);
 	}
-	
+
 	public static class CifraBase64 {
 		private static Base64 cripto;
-		
+
 		static {
 			if (cripto == null)
 				cripto = new Base64();
@@ -64,129 +97,130 @@ public class CriptoHelper {
 		public static String descriptografar(String cifroTexto) {
 			return new String(cripto.decode(cifroTexto.getBytes()));
 		}
-		
+
 		public static void main(String[] args) throws Exception {
-			String puroTexto = "Este é o puro texto";	
+			String puroTexto = "Este é o puro texto";
 			System.out.println("Texto: " + puroTexto);
-			
+
 			System.out.print("Criptografar: ");
 			String cifroTexto = CifraBase64.criptografar(puroTexto);
 			System.out.println(cifroTexto.toString());
-			
+
 			System.out.print("Decriptografar: ");
 			String novoPuroTexto = CifraBase64.descriptografar(cifroTexto);
 			System.out.println(novoPuroTexto);
 		}
 	}
-	
-	
+
 	public static class CifraSimetrica {
-		//https://docs.oracle.com/javase/7/docs/technotes/guides/security/StandardNames.html#Cipher
-		private static final String ALGORITIMO = "RC4"; //"AES";
-		
+		// https://docs.oracle.com/javase/7/docs/technotes/guides/security/StandardNames.html#Cipher
+		private static final String ALGORITIMO = "RC4"; // "AES";
+
 		public static String criptografar(String puroTexto, String chave) throws Exception {
-			//pegar chave
+			// pegar chave
 			SecureRandom sr = new SecureRandom(chave.getBytes());
-			
+
 			KeyGenerator geradorChave = KeyGenerator.getInstance(ALGORITIMO);
-						 geradorChave.init(sr);
-						 
+			geradorChave.init(sr);
+
 			SecretKey chaveSecreta = geradorChave.generateKey();
-	
-			//criptografar
+
+			// criptografar
 			Cipher cifra = Cipher.getInstance(ALGORITIMO);
-	               cifra.init(Cipher.ENCRYPT_MODE, chaveSecreta);
-	
+			cifra.init(Cipher.ENCRYPT_MODE, chaveSecreta);
+
 			byte[] arrBytes = cifra.doFinal(puroTexto.getBytes());
-			
-			//tranformar array de bytes em um bloco de texto
+
+			// tranformar array de bytes em um bloco de texto
 			String cifroTexto = "";
 			for (byte b : arrBytes) {
 				cifroTexto += b + "|";
 			}
-			cifroTexto = cifroTexto.substring(0, cifroTexto.length() -1);
+			cifroTexto = cifroTexto.substring(0, cifroTexto.length() - 1);
 			return cifroTexto;
 		}
-	
+
 		public static String descriptografar(String cifroTexto, String chave) throws Exception {
-			//transformar bloco de texto em array de bytes
-			String [] arrString = cifroTexto.split("\\|");
+			// transformar bloco de texto em array de bytes
+			String[] arrString = cifroTexto.split("\\|");
 			byte[] arrBytes = new byte[arrString.length];
 			for (int i = 0; i < arrString.length; i++) {
 				arrBytes[i] = Byte.parseByte(arrString[i]);
 			}
-			
-			//pegar chave
+
+			// pegar chave
 			SecureRandom sr = new SecureRandom(chave.getBytes());
-			
+
 			KeyGenerator geradorChave = KeyGenerator.getInstance(ALGORITIMO);
-						 geradorChave.init(sr);
-						 
+			geradorChave.init(sr);
+
 			SecretKey chaveSecreta = geradorChave.generateKey();
-	
-			//descriptografar
+
+			// descriptografar
 			Cipher cifra = Cipher.getInstance(ALGORITIMO);
-				   cifra.init(Cipher.DECRYPT_MODE, chaveSecreta);
-			
+			cifra.init(Cipher.DECRYPT_MODE, chaveSecreta);
+
 			byte[] descriptografar = cifra.doFinal(arrBytes);
-	
+
 			return new String(descriptografar);
 		}
-		
+
 		public static void main(String[] args) throws Exception {
-			String puroTexto = "Este é o puro texto";	
+			String puroTexto = "Este é o puro texto";
 			System.out.println("Texto: " + puroTexto);
-			
+
 			System.out.print("Criptografar: ");
 			String cifroTexto = CifraSimetrica.criptografar(puroTexto, "CHAVE");
 			System.out.println(cifroTexto.toString());
-			
+
 			System.out.print("Decriptografar: ");
 			String novoPuroTexto = CifraSimetrica.descriptografar(cifroTexto, "CHAVE");
 			System.out.println(novoPuroTexto);
 		}
 	}
-	
-	
+
 	public static class CifraAssimetrica {
-		//https://docs.oracle.com/javase/7/docs/technotes/guides/security/StandardNames.html#Cipher
-		private static final String ALGORITIMO = "RSA";  //pode ser RSA, 
-		
-	    public static KeyPair gerarParChaves(String chave) throws NoSuchAlgorithmException {
+		// https://docs.oracle.com/javase/7/docs/technotes/guides/security/StandardNames.html#Cipher
+		private static final String ALGORITIMO = "RSA";
+
+		public static KeyPair gerarParChaves(String chave) throws NoSuchAlgorithmException {
 			SecureRandom sr = new SecureRandom(chave.getBytes());
-	        KeyPairGenerator gerador = KeyPairGenerator.getInstance("RSA");
-	        				 gerador.initialize(512, sr); //512 bytes é o tamanho minimo dessa chave para esta cifra
-	        				 
-	        KeyPair parChaves = gerador.generateKeyPair();
-	
-	        return parChaves;
-	    }
-	
-	    public static byte[] criptografar(String puroTexto, PublicKey chavePublica) throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException {
-	        Cipher cifra = Cipher.getInstance(ALGORITIMO);
-	        	   cifra.init(Cipher.ENCRYPT_MODE, chavePublica);
-	        	   
-	        return cifra.doFinal(puroTexto.getBytes());
-	    }
-	
-	    public static String descriptografar(byte[] cifroTexto, PrivateKey chavePrivada) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IOException, BadPaddingException, IllegalBlockSizeException {
-	        Cipher cifra = Cipher.getInstance(ALGORITIMO);
-	        	   cifra.init(Cipher.DECRYPT_MODE, chavePrivada);
-	        	   
-	        return new String(cifra.doFinal(cifroTexto));
-	    }
-		
+			KeyPairGenerator gerador = KeyPairGenerator.getInstance("RSA");
+			gerador.initialize(512, sr); // 512 bytes é o tamanho minimo dessa chave para esta cifra
+
+			KeyPair parChaves = gerador.generateKeyPair();
+
+			return parChaves;
+		}
+
+		public static byte[] criptografar(String puroTexto, PublicKey chavePublica) throws InvalidKeyException,
+				NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException {
+			Cipher cifra = Cipher.getInstance(ALGORITIMO);
+			cifra.init(Cipher.ENCRYPT_MODE, chavePublica);
+
+			return cifra.doFinal(puroTexto.getBytes());
+		}
+
+		public static String descriptografar(byte[] cifroTexto, PrivateKey chavePrivada)
+				throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IOException,
+				BadPaddingException, IllegalBlockSizeException {
+			Cipher cifra = Cipher.getInstance(ALGORITIMO);
+			cifra.init(Cipher.DECRYPT_MODE, chavePrivada);
+
+			return new String(cifra.doFinal(cifroTexto));
+		}
+
 		public static void main(String[] args) throws Exception {
 			String puroTexto = "Este é o puro texto";
-			
+
 			KeyPair parChaves = CifraAssimetrica.gerarParChaves("CHAVE");
-			
+
 			System.out.println("Texto: " + puroTexto);
-			
+
 			System.out.print("Criptografar: ");
 			byte[] cifroTexto = CifraAssimetrica.criptografar(puroTexto, parChaves.getPublic());
 			System.out.println(cifroTexto);
-			
+
 			System.out.print("Decriptografar: ");
 			String novoPuroTexto = CifraAssimetrica.descriptografar(cifroTexto, parChaves.getPrivate());
 			System.out.println(novoPuroTexto);
